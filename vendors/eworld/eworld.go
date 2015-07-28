873e81cf823e909171e594d1046ad1012cc17f3c
lzbgt@126.com <lzbgt@126.com>
2015-07-28 12:57:14 +0800

    revised

--- vendors/eworld/eworld.go
+++ vendors/eworld/eworld.go
@@ -259,7 +259,7 @@ HANDLED_CMD:
 func (s *EWorld) parseMessage(parts []string, conn *net.Conn) interface{} {
 	var err error = nil
 	var lat, lng float64
-	if len(parts) < 3 || len(parts[2]) < 1 || len(parts[1] < 1) {
+	if len(parts) < 3 || len(parts[2]) < 1 || len(parts[1]) < 1 {
 		return
 	}
 
