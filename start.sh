cdb798c28f017c7b996c9d25c48beb3eabe643e3
lzbgt@126.com <lzbgt@126.com>
2015-08-04 09:44:36 +0800

    EWORLD GPS Revised

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nohup ./lbsas -log=info -dbmoc=20 -dbmic=10 -dbaddr="tusung:tusung123@tcp(rdsrrmqumrrmqum.mysql.rds.aliyuncs.com:3306)/cargts" -dbcachesize=800000 -tcpaddr="0.0.0.0:8082" -httpaddr="0.0.0.0:8083" >log.txt 2>&1 &
+nohup ./lbsas -log=warn -dbmoc=10 -dbmic=10 -dbaddr="tusung:tusung123@tcp(121.43.226.205:63306)/cargts" -dbcachesize=800000 -tcpaddr="0.0.0.0:9020" -httpaddr="0.0.0.0:9021" -lbs="http://127.0.0.1:9001/api/lbs" >>lbsas.log 2>&1 &
